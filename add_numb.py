362a09e9daed1019226264b97fdf8948173aeb2e
Jonathan Wren <jdwren@gmail.com>
2013-07-01 15:47:38 -0400

    made the final version

--- add_numb.py
+++ add_numb.py
@@ -1,9 +1,13 @@
 #This is a comment line
+from adding import add_three
+
 def add_two(a,b):
+	print("adding 2 nums")
 	return a+b
 
 def main():
 	print ("2+3=", add_two(2,3))
+	print ("2+2+5=", add_three(2,2,5))
 
 if __name__ == "__main__":
 	main()
